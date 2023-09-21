b702f592cd02134c1187ec514ecab5efa78b81ba
Mattias Rost <rrostt@gmail.com>
2023-09-21 06:27:50 +0000

    Formats code.

--- components/AddTodoForm.js
+++ components/AddTodoForm.js
@@ -1,17 +1,21 @@
-import { useState } from 'react';
-import styles from '../styles/home.module.css';
-import Button from './Button';
+import { useState } from 'react'
+import styles from '../styles/home.module.css'
+import Button from './Button'
 
 export const AddTodoForm = ({ onAdd }) => {
-  const [text, setText] = useState('');
+  const [text, setText] = useState('')
 
   const onClick = () => {
-    onAdd({ text: text });
-    setText('');
-  };
+    onAdd({ text: text })
+    setText('')
+  }
 
   return <div className={styles.column}>
-    <input style={{ margin: '10px 0' }} type='text' value={text} onChange={e => setText(e.target.value)} />
+    <input 
+      style={{ margin: '10px 0' }} 
+      type='text' value={text} 
+      onChange={e => setText(e.target.value)} 
+    />
     <Button onClick={onClick}>Add todo</Button>
-  </div>;
-};
+  </div>
+}
